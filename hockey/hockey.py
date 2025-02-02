37eb6951c7b7490b5adbfd94eca142c62cb4035d
AdirtKaLaptop <AdirtKa@yandex.ru>
2025-02-02 14:18:31 +1000

    add parsing table on a page to a dict format

--- hockey/hockey.py
+++ hockey/hockey.py
@@ -1,5 +1,7 @@
 """Parsing hockey site."""
 from logging import Logger
+from pprint import pprint
+from typing import Coroutine
 from urllib.parse import urljoin
 import asyncio
 
@@ -36,11 +38,13 @@ async def get_pages_count(session: ClientSession, per_page: int = 25) -> int:
 
         if pagination.text == "\n" or len(pagination.find_all("li")) < 2:
             # Пагинация не найдена или есть только одна страница
+            logger.info("Successfully fetched 1 page")
             return 1
 
         # Извлекаем количество страниц
-        pages_count = pagination.find_all("li")[-2].text.strip()
-        return int(pages_count)
+        pages_count = int(pagination.find_all("li")[-2].text.strip())
+        logger.info("Successfully fetched %s pages", pages_count)
+        return pages_count
 
     except asyncio.TimeoutError as e:
         logger.error("Timeout error for %s: %s", url, e)
@@ -55,12 +59,61 @@ async def get_pages_count(session: ClientSession, per_page: int = 25) -> int:
         return -1
 
 
+async def parse_page(session: ClientSession, per_page: int, page_num: int) -> list[dict]:
+    """
+    Parse all table data from a page.
+
+    :param session: aiohttp session
+    :param per_page: how many rows per page
+    :param page_num: page index
+    :return:
+    """
+    url: str = urljoin(BASE_URL, f"?{page_num=}&{per_page=}")
+    logger.info("Parsing page: %s", url)
+    async with session.get(url) as response:
+        text = await response.text()
+
+    soup: BeautifulSoup = BeautifulSoup(text, "lxml")
+    soup = soup.find("table", class_="table")
+    rows: list[BeautifulSoup] = soup.find_all("tr")
+    logger.info("Found %s rows", len(rows) - 1)
+
+    table_headers_soup: list[BeautifulSoup] = rows[0].find_all("th")
+    table_headers: list[str] = [column.text.strip() for column in table_headers_soup]
+
+    rows_without_headers: list[BeautifulSoup] = rows[1:]
+    rows_data: list[dict] = []
+    for row in rows_without_headers:
+        row_data: dict = parse_row(row, table_headers)
+        rows_data.append(row_data)
+
+    return rows_data
+
+
+def parse_row(row: BeautifulSoup, table_headers: list[str]) -> dict[str, str]:
+    """
+    Transform soup row into json format
+
+    :param row: BeautifulSoup row
+    :param table_headers: list of table headers
+    :return: json format row
+    """
+
+    columns_soup: list[BeautifulSoup] = row.find_all("td")
+    columns: list[str] = [column.text.strip() for column in columns_soup]
+    return dict(zip(table_headers, columns))
+
+
 async def main():
     """Entry point."""
     async with ClientSession() as session:
-        pages_count: int = await get_pages_count(session, per_page=600)
-        logger.info("Successfully fetched %s pages", pages_count)
-        print(pages_count)
+        per_page: int = 25
+        pages_count: int = await get_pages_count(session, per_page=per_page)
+        tasks: list[Coroutine] = [parse_page(session, per_page, i) for i in range(1, pages_count + 1)]
+
+        results: list[list[dict]] = await asyncio.gather(*tasks)
+        pprint(results)
+
 
 
 if __name__ == '__main__':
